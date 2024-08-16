f08fa800f28ee0204798e1f0f7850db3821ba330
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-08-16 13:44:24 +0700

    create inventory on register

--- src/main/java/zurihaqi/simple_gacha_sim/seeder/AdminSeeder.java
+++ src/main/java/zurihaqi/simple_gacha_sim/seeder/AdminSeeder.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import zurihaqi.simple_gacha_sim.model.Inventory;
 import zurihaqi.simple_gacha_sim.model.User;
+import zurihaqi.simple_gacha_sim.repository.InventoryRepository;
 import zurihaqi.simple_gacha_sim.repository.UserRepository;
 
 import java.util.Date;
@@ -15,6 +17,7 @@ import java.util.Date;
 @Transactional
 public class AdminSeeder implements CommandLineRunner {
     private final UserRepository userRepository;
+    private final InventoryRepository inventoryRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -26,6 +29,13 @@ public class AdminSeeder implements CommandLineRunner {
         String adminEmail = "admin@mail.com";
         String adminPassword = "admin1234";
         if(userRepository.findByEmail(adminEmail).isPresent()) return;
+
+        inventoryRepository.save(
+                Inventory.builder()
+                        .createdAt(new Date())
+                        .updatedAt(null)
+                        .build()
+        );
 
         userRepository.save(
                 User.builder()
